a44da76ee04cc15c2b4beda3d2a61e2b6d66cb41
haidi <101851741+Haidi927@users.noreply.github.com>
2025-06-20 15:09:05 +0800

    gnn.py

---
+++ model/gnn.py
@@ -0,0 +1,15 @@
+import torch
+from torch_geometric.nn import GCNConv
+import torch.nn.functional as F
+import torch.nn as nn
+
+class RoadGNN(nn.Module):
+    def __init__(self, in_channels, hidden_channels):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.conv1(x, edge_index))
+        x = self.conv2(x, edge_index)
+        return x
